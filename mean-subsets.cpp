ebaf4a91ba15f195b640f6ad328e01ddd2322a9c
Dan Park <dpark.sports@gmail.com>
2020-07-18 15:52:37 -0700

    Create mean-subsets.cpp

---
+++ mean-subsets.cpp
@@ -0,0 +1,49 @@
+//Write a function which takes series of numbers and removes duplicates and returns unique numbers.
+
+vector<int> uniqueNumbers(vector<int> numbers) {
+map<int> uniquenumbers;
+
+for (int i = 0; i < numbers.size(); i++) {
+	uniquenumbers[numbers[i] = numbers[i];
+}
+
+return uniquenumbers.first().list()
+
+}
+
+//Write a function which takes series of numbers and given average . Find all the subset of numbers which can produce given average and 
+//return all the subsets
+
+total_subset 
+count_subset
+
+avg = total_subset / count_subset
+numbers = [1,2,3,4,5]
+bags = [1,2], [1,3], [1,4], [1,5],
+       [2,1],
+map key = subset hash
+map[key] = subset avg
+
+traverse of a tree
+a tree branch[3] = [1,2,3], [1,2,4], [1,2,5],
+eg. map key of [1,2,3] = set(1,2,3) hash key = set (3,2,1) hash key
+
+a tree branch[2] = [1,2], [1,3], [1,4], [1,5]
+eg. map key of [1,2] = set(1,2) hash key = set (2,1) hash key
+
+traverse a tree of total of 5 numbers.
+root node (n=5) = set (1,2,3,4,5)
+branch node (n=4) = set (1,2,3,4), set(1,2,3,5) 
+branch node (n=3) =set (1,2,3), set(1,2,4), set(1,2,5) 
+
+struct node {
+  set(n)
+  total_count;
+  total_subset;
+}
+
+vector<int> subset(vector<int> numbers) {
+
+
+}
+
